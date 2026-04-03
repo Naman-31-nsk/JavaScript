4bf303df4cd47aed0f16f5ed61db25e441ad7dfc
Naman-31-nsk <codebreakernaman@gmail.com>
2026-04-03 23:47:33 +0530

    Create even_odd.js

---
+++ even_odd.js
@@ -0,0 +1,10 @@
+
+let number = 10;
+
+
+const checkEvenOddArrow = (n) => {
+    return (n % 2 === 0) ? "Even" : "Odd";
+};
+
+console.log("Number:", number);
+console.log("Result:", checkEvenOddArrow(number));
